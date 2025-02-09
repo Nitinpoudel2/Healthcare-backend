a16fad407a572b5218575b1b8687ce74f264f7c7
Nitin-Paudel <nitinpoudel2071@gmail.com>
2025-02-09 00:02:49 -0600

    Initial Commit

---
+++ src/test/java/HealthCare_Backend/Healthcare/HealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package HealthCare_Backend.Healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
